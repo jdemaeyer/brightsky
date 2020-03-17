350715f950ef71ac191a5cd8065e721aea77824d
Jakob de Maeyer <jakobdm1@gmail.com>
2020-03-17 10:48:10 +0100

    Add CI
    Closes #3

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='brightsky',
+    version='0.1a',
+    author='Jakob de Maeyer',
+    author_email='jakob@naboa.de',
+    packages=['brightsky'],
+    install_requires=[
+        'parsel',
+        'python-dateutil',
+        'requests',
+    ],
+)
