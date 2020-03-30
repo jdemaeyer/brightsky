809adc45ed6ddb87a4747f4848939c9339567f71
Jakob de Maeyer <jakobdm1@gmail.com>
2020-03-30 15:31:21 +0200

    Add crude database migrations
    Closes #15

---
+++ brightsky/db.py
@@ -0,0 +1,44 @@
+import glob
+import logging
+import os
+import re
+
+import psycopg2
+
+
+logger = logging.getLogger(__name__)
+
+
+def migrate():
+    logger.info("Migrating database")
+    with psycopg2.connect(os.getenv('DATABASE_URL')) as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute('SELECT MAX(id) FROM migrations;')
+            except psycopg2.errors.UndefinedTable:
+                conn.rollback()
+                latest_migration = 0
+            else:
+                latest_migration = cur.fetchone()[0]
+
+            migration_paths = [
+                f for f in glob.glob('migrations/*.sql')
+                if (m := re.match(r'(\d+)_', os.path.basename(f)))
+                and int(m.group(1)) > latest_migration
+            ]
+
+            for path in migration_paths:
+                logger.info("Applying %s", path)
+                match = re.match(r'(\d+)_?(.*)\.sql', os.path.basename(path))
+                migration_id = int(match.group(1))
+                migration_name = match.group(2)
+                with open(path) as f:
+                    cur.execute(f.read())
+                cur.execute(
+                    'INSERT INTO migrations (id, name) VALUES (%s, %s);',
+                    (migration_id, migration_name))
+                conn.commit()
+        if migration_paths:
+            logger.info("Applied %d migrations", len(migration_paths))
+        else:
+            logger.info("No new migrations")
--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     install_requires=[
         'coloredlogs',
         'parsel',
+        'psycopg2-binary',
         'python-dateutil',
         'requests',
     ],
