855f7db15d78affde856d9394bbe4c5915620ada
Jakob de Maeyer <jakobdm1@gmail.com>
2020-08-25 15:10:28 +0200

    Allow all origins at status endpoint

--- brightsky/web.py
+++ brightsky/web.py
@@ -221,6 +221,8 @@ class SourcesResource(BrightskyResource):
 
 class StatusResource:
 
+    cors = falcon_cors.CORS(allow_all_origins=True)
+
     def on_get(self, req, resp):
         resp.media = {
             'name': 'brightsky',
