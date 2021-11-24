936ebfff19e2c8137a3577677ab9974544d07193
Jakob de Maeyer <jakobdm1@gmail.com>
2021-11-24 20:50:02 +0100

    Allow supplying Sentry Performance sample rate as environment variable

--- brightsky/__main__.py
+++ brightsky/__main__.py
@@ -9,5 +9,10 @@ if __name__ == '__main__':
     configure_logging()
     if os.getenv('SENTRY_DSN'):
         import sentry_sdk
-        sentry_sdk.init(os.getenv('SENTRY_DSN'))
+        sample_rate = os.getenv('SENTRY_TRACES_SAMPLE_RATE')
+        if sample_rate:
+            sample_rate = float(sample_rate)
+        sentry_sdk.init(
+            dsn=os.getenv('SENTRY_DSN'),
+            traces_sample_rate=sample_rate)
     cli(prog_name='python -m brightsky')
