d86725bfa08b721c36d1c85fde4eee61118ee62c
Jakob de Maeyer <jakobdm1@gmail.com>
2023-05-05 10:15:55 +0200

    Allow enabling Sentry Profiling

--- brightsky/__main__.py
+++ brightsky/__main__.py
@@ -4,15 +4,19 @@ from brightsky.cli import cli
 from brightsky.utils import configure_logging, load_dotenv
 
 
+def _getenv_float(key):
+    x = os.getenv(key)
+    return float(x) if x else None
+
+
 if __name__ == '__main__':
     load_dotenv()
     configure_logging()
     if os.getenv('SENTRY_DSN'):
         import sentry_sdk
-        sample_rate = os.getenv('SENTRY_TRACES_SAMPLE_RATE')
-        if sample_rate:
-            sample_rate = float(sample_rate)
         sentry_sdk.init(
             dsn=os.getenv('SENTRY_DSN'),
-            traces_sample_rate=sample_rate)
+            traces_sample_rate=_getenv_float('SENTRY_TRACES_SAMPLE_RATE'),
+            profiles_sample_rate=_getenv_float('PROFILES_TRACES_SAMPLE_RATE'),
+        )
     cli(prog_name='python -m brightsky')
